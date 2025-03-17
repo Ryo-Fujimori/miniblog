abea392085049d1711db0f02b2daaa3a47f0430b
Ryo-Fujimori <fujimori1072@gmail.com>
2025-03-17 19:42:48 +0900

    フォームの修正
    パラメータを渡すためのアクションpost_paramを作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,23 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.order(:id).limit(10)
+  end
+
+  def new
     @new_post = Post.new
   end
+
+  def create
+    @new_post = Post.new(post_params)
+    pp @new_post
+    @new_post.save
+    redirect_to '/'
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
 end
