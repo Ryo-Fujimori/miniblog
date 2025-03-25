de00f60a44a75e663d063b7e990a5ad8b4322ade
Ryo-Fujimori <fujimori1072@gmail.com>
2025-03-25 17:04:04 +0900

    カスタマイズしたビューへのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   root 'posts#index'
   resources :posts, only: %i[index new create edit update]
   get 'up' => 'rails/health#show', as: :rails_health_check
