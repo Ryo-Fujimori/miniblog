8d44cf2d2e407b4544912d5b5d6c00b1a4bec308
Ryo-Fujimori <fujimori1072@gmail.com>
2025-04-05 12:28:54 +0900

    マイグレーションファイル修正

--- db/migrate/20250404140956_create_relationships.rb
+++ db/migrate/20250404140956_create_relationships.rb
@@ -2,6 +2,8 @@ class CreateRelationships < ActiveRecord::Migration[8.0]
   def change
     create_table :relationships do |t|
       t.timestamps
+      t.integer :follower_id
+      t.integer :followed_id
     end
     add_foreign_key :relationships, :users, column: :followed_id
     add_foreign_key :relationships, :users, column: :follower_id
