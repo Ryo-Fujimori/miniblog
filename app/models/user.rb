302f69eae6096345898fb0eab99e2afc82185f2e
Ryo-Fujimori <fujimori1072@gmail.com>
2025-04-05 15:46:56 +0900

    新たにrelationshipを作成するアクションをUserモデルに作成(buildやcreateと同じ役割のアクションのため)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :email, presence: true
-  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
-  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
+  
+  def follow(user)
+    active_relationships.create(followed_id: user.id)
+  end
 end
