26202a71146d42cb74688ad4fc8e2f4ed5d6902a
Ryo-Fujimori <fujimori1072@gmail.com>
2025-04-05 14:44:57 +0900

    userモデルとrelationshipモデルを関連付けする

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :email, presence: true
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
 end
