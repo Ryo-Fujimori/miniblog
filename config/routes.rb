c621dae17b0f9a5d601e1e1d2a73b30ba5cff929
Ryo-Fujimori <fujimori1072@gmail.com>
2025-04-06 17:45:55 +0900

    User一覧画面、indexアクションを作成
    rails g controller Users index

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   root 'posts#index'
   resources :posts, only: %i[index new create show edit update destroy]
+  get "users/index"
   get 'up' => 'rails/health#show', as: :rails_health_check
 end
