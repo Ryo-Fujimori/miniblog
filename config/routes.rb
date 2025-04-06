1f470c0293d55ed925ce8bb29604cd44997f3b61
Ryo-Fujimori <fujimori1072@gmail.com>
2025-04-06 16:53:17 +0900

    relationshipのコントローラーを作成、createアクションを作成
    rails g controller Relationships create

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "relationships/create"
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   root 'posts#index'
   resources :posts, only: %i[index new create show edit update destroy]
