ecc4d7cb8f7c27c22870b10bc773aafbb19adc60
Ryo-Fujimori <fujimori1072@gmail.com>
2025-04-06 18:42:40 +0900

    User一覧画面から呼び出す方法が不明のためUserコントローラーでcreateアクション作成

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,12 +1,12 @@
 class RelationshipsController < ApplicationController
   before_action :authenticate_user!, only: %i[create]
 
-  def create
-    @user = User.find(params[:id])
-    if current_user.follow(@user)
-      redirect_to users_index_path
-    else
-      redirect_to users_index_path, notice: 'フォロー失敗'
-    end
-  end
+  # def create →User一覧画面から呼び出す方法が不明のためUserコントローラーで作成
+  #   @user = User.find(params[:id])
+  #   if current_user.follow(@user)
+  #     redirect_to users_index_path
+  #   else
+  #     redirect_to users_index_path, notice: 'フォロー失敗'
+  #   end
+  # end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: %i[create]
   def index
     @users = User.order(id: :desc).limit(10)
+  end
+
+  def create
+    @user = User.find(params[:id])
+    if current_user.follow(@user)
+      redirect_to users_index_path
+    else
+      redirect_to users_index_path, notice: 'フォロー失敗'
+    end
   end
 end
