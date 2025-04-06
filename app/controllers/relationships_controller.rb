256e6cb47b5004c9effbb84235d215939282820b
Ryo-Fujimori <fujimori1072@gmail.com>
2025-04-06 17:44:41 +0900

    createアクションを作成

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,4 +1,12 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!, only: %i[create]
+
   def create
+    @user = User.find(params[:id])
+    if current_user.follow(@user)
+      redirect_to users_index_path
+    else
+      redirect_to users_index_path, notice: 'フォロー失敗'
+    end
   end
 end
