b668bfac5057671dbdf9364e2eb5dcab6b9b1cad
Ryo-Fujimori <fujimori1072@gmail.com>
2025-04-07 21:27:49 +0900

    relationshipコントローラーにdestroyアクションを作成

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,12 +1,18 @@
 class RelationshipsController < ApplicationController
-  before_action :authenticate_user!, only: %i[create]
+  before_action :authenticate_user!, only: %i[create destroy]
 
   def create
-    @user = User.find(params[:id])
-    if current_user.follow(@user)
+    user = User.find(params[:user_id])
+    if current_user.follow(user)
       redirect_to users_path, notice: 'フォローしました'
     else
       redirect_to users_path, alert: 'フォロー失敗'
     end
+  end
+
+  def destroy
+    relationship = current_user.active_relationships.find(params[:id])
+    relationship.destroy!
+    redirect_to users_path, notice: 'フォロー解除しました'
   end
 end
