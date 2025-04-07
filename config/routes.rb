003e35d4dad2c235d4a6fbbc47bd4e5f8686c08e
Ryo-Fujimori <fujimori1072@gmail.com>
2025-04-07 21:28:01 +0900

    ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   resources :users, only: %i[index create]
   resources :posts, only: %i[index new create show edit update destroy]
-  resources :relationships, only: %i[create]
+  resources :relationships, only: %i[create destroy]
   get 'up' => 'rails/health#show', as: :rails_health_check
 end
