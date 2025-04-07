c316dd91ca2862d890716641c50c4c8d05c31594
Ryo-Fujimori <fujimori1072@gmail.com>
2025-04-07 20:17:05 +0900

    リレーションシップにcreateアクション作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,7 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: %i[create index]
+  before_action :authenticate_user!, only: %i[index]
   def index
     @users = User.order(id: :desc).limit(10)
   end
 
-  def create
-    @user = User.find(params[:id])
-    if current_user.follow(@user)
-      redirect_to users_path
-    else
-      redirect_to users_path, notice: 'フォロー失敗'
-    end
-  end
 end
